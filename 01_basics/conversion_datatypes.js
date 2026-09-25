e5dd8430b43bfadee98955743b7dac1cdbef09c9
Aleezay Imran <alizeimran95@gmail.com>
2026-09-25 20:18:48 +0000

    data type conversion

---
+++ 01_basics/conversion_datatypes.js
@@ -0,0 +1,27 @@
+let score = "ezay"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+/*let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+// numbers are easily convert into number 
+// when this "45abc" it gives NAN
+// true convert into 1  false convert into 0
+
+let isLoggedIn = "g"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn)
+
+// 1 converts into true
+// 0 converts into false 
+// "" gives false
+// "ezay" gives true 
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
