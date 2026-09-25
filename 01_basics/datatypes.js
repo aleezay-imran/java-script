a57a6a6855c18c6bd85438f5d1a365d795e1135d
Aleezay Imran <alizeimran95@gmail.com>
2026-09-25 19:34:43 +0000

    data types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // as a newer version 
+
+
+// alert("hello")  as uded node js not browser 
+
+console.log (3 +3 )
+console.log ("ezay")
+let name = "ezay"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false 
+// null => standalone value 
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
